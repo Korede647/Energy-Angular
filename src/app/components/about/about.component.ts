6243608f8f4606c221f0ff8a64df86dcf423e202
Korede Anunusor <koredeanunusor@gmai.com>
2025-04-22 14:57:02 +0100

    new additions

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { heroBolt } from '@ng-icons/heroicons/outline'
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [
+    NgIcon
+  ],
+  viewProviders: [provideIcons({
+    heroBolt
+  })],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
